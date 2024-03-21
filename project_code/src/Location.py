49960271bb32b3a7f8f897cde5367f236db3d16a
fahmed79 <150975352+fahmed79@users.noreply.github.com>
2024-03-21 17:51:01 -0500

    Update Location.py

--- project_code/src/Location.py
+++ project_code/src/Location.py
@@ -1,2 +1,11 @@
 class Location:
-    pass
+
+    def create_custom_event_from_static_text_files(self):
+        with open("path/to/file.txt". "r") as file:
+        data = json.load(file)
+
+file.primary = data['primary_attribute]
+"secondary_attribute" = "decterity"
+"promote _text" : "A dragon appears, what will you do"
+"fail" pass: {
+    message: "You failed"
