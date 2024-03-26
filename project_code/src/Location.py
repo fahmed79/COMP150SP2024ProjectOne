c01813a3f6cd70d700e0c0ec54c2562b93c8fba0
nashi <hle8@luc.edu>
2024-03-26 05:35:53 -0700

    fixed location

--- project_code/src/Location.py
+++ project_code/src/Location.py
@@ -1,11 +1,15 @@
+import json
+
 class Location:
-
     def create_custom_event_from_static_text_files(self):
-        with open("path/to/file.txt". "r") as file:
-        data = json.load(file)
+        with open("path/to/file.txt", "r") as file:
+            data = json.load(file)
 
-file.primary = data['primary_attribute]
-"secondary_attribute" = "decterity"
-"promote _text" : "A dragon appears, what will you do"
-"fail" pass: {
-    message: "You failed"
+        event = Event()  # Assuming Event class exists
+        event.primary = data['primary_attribute']
+        event.secondary = "dexterity"  
+        event.prompt_text = "A dragon appears, what will you do"  
+        event.fail = {"message": "You failed"}  
+        event.pass_ = {"message": "You passed"}  
+
+        return event
